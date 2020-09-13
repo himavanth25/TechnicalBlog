4b2f3965c7247f0c8971daff77f9b3730ea7dca2
Himavanth Kanumalla <himavanth25@gmail.com>
2020-09-13 12:19:19 +0530

    2nd commit seperating the service implementation, added PostService

--- src/main/java/com/upgrad/controller/HomeController.java
+++ src/main/java/com/upgrad/controller/HomeController.java
@@ -1,51 +1,35 @@
 package com.upgrad.controller;
 
 import com.upgrad.model.Post;
+import com.upgrad.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 @Controller
 public class HomeController {
-
-   @RequestMapping("/")
-    public ModelAndView getAllPosts(ModelAndView model) {
-
-        ArrayList<Post> posts = new ArrayList<>();
-
-        Post post1 = new Post();
-        post1.setTitle("Post 1");
-        post1.setBody("Post Body 1");
-        post1.setDate(new Date());
-
-        Post post2 = new Post();
-        post2.setTitle("Post 2");
-        post2.setBody("Post Body 2");
-        post2.setDate(new Date());
-
-        Post post3 = new Post();
-        post3.setTitle("Post 3");
-        post3.setBody("Post Body 3");
-        post3.setDate(new Date());
-
-        posts.add(post1);
-        posts.add(post2);
-        posts.add(post3);
-
-        model.addObject("posts", posts);
-        model.setViewName("index");
-        return model;
-
-
+    public HomeController(){
+        System.out.println("***Home Controller is called. ***");
     }
 
-    @RequestMapping(value="/sayHello")
+    @Autowired
+    private PostService postService;
+
+    @RequestMapping("/")
+    public String getAllPosts(Model model) {
+
+        ArrayList<Post> posts = postService.getAllPosts();
+        model.addAttribute("posts", posts);
+        return "index";
+    }
+
+    @RequestMapping(value = "/sayHello")
     @ResponseBody
-    public String getString(){
+    public String getString() {
         return "Hello";
     }
 
